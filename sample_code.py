1f15294a713e6a70d001a88ec44d0cf66448a9ed
Hsiang-Jen Li <hsiangjenli@gmail.com>
2023-12-29 16:17:23 +0800

    feat: sample code and update google drive id

--- sample_code.py
+++ sample_code.py
@@ -1,12 +1,9 @@
 from temporal_graph import TemporalGraph
 
-tg_data = TemporalGraph(root="data2")
+tg_data = TemporalGraph(root="data")
 
-data = tg_data("tgbl-coin")
-print(data)
-
-# for dataset_name in tg_data.available_datasets:
-#     print(f"Downloading {dataset_name}...")
-#     data = tg_data(dataset_name)
-#     print(data)
-#     print(f"Downloaded {dataset_name}!")
+for dataset_name in tg_data.available_datasets:
+    print(f"Downloading {dataset_name}...")
+    data = tg_data(dataset_name)
+    print(data)
+    print(f"Downloaded {dataset_name}!")
