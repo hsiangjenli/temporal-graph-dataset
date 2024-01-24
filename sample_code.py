b69a2ba6b4fb6f3df0b7cd799e6326af0a116a53
Hsiang-Jen Li <hsiangjenli@gmail.com>
2024-01-24 13:28:00 +0800

    feat: pseudo data

--- sample_code.py
+++ sample_code.py
@@ -1,15 +1,14 @@
 from temporal_graph import TemporalGraphDataset
 from torch_geometric.loader import TemporalDataLoader
-import numpy as np
 
-tgd = TemporalGraphDataset(root="data_demo")
+tgd = TemporalGraphDataset(root="data")
 
 for dataset_name in tgd.available_datasets:
     print(f"Downloading {dataset_name}...")
     tgd.download(dataset_name)
     print(f"Downloaded {dataset_name}!")
 
-data, x, train_mask, val_mask, test_mask = tgd("eed")
+data, x, train_mask, val_mask, test_mask = tgd("pseudo_data")
 print(data)
 print(x)
 print(data[train_mask])
