5f13123121303b58547d949c64579bffd0027e98
Hsiang-Jen Li <hsiangjenli@gmail.com>
2024-04-05 13:15:48 +0800

    feat: add available datasets (dblp-syear-eyear)  and update Makefile

--- Makefile
+++ Makefile
@@ -2,12 +2,20 @@ process:
 	# python temporal_graph/preprocess/p_flight.py
 	# python temporal_graph/preprocess/p_coin.py
 	# python temporal_graph/preprocess/p_dblp.py
-	python temporal_graph/preprocess/p_enron.py
+	# python temporal_graph/preprocess/p_enron.py
+	python temporal_graph/preprocess/p_dblp_year.py --start_year 1936 --end_year 2021
+	python temporal_graph/preprocess/p_dblp_year.py --start_year 1936 --end_year 2022
+	python temporal_graph/preprocess/p_dblp_year.py --start_year 1936 --end_year 2023
+	python temporal_graph/preprocess/p_dblp_year.py --start_year 1936 --end_year 2024
 
 backup:
-	python test_backup.py --input_folder data/tgbl-coin --gdrive_path "/Users/hsiangjenli/Google Drive/My Drive/🏫 2022 NTUST/📦 data/pyg/tgbl-coin"
-	python test_backup.py --input_folder data/tgbl-flight --gdrive_path "/Users/hsiangjenli/Google Drive/My Drive/🏫 2022 NTUST/📦 data/pyg/tgbl-flight"
-	python test_backup.py --input_folder data/eed --gdrive_path "/Users/hsiangjenli/Google Drive/My Drive/🏫 2022 NTUST/📦 data/pyg/eed"
-	python test_backup.py --input_folder data/dblp --gdrive_path "/Users/hsiangjenli/Google Drive/My Drive/🏫 2022 NTUST/📦 data/pyg/dblp"
+	# python test_backup.py --input_folder data/tgbl-coin --gdrive_path "/Users/hsiangjenli/Google Drive/My Drive/🏫 2022 NTUST/📦 data/pyg/tgbl-coin"
+	# python test_backup.py --input_folder data/tgbl-flight --gdrive_path "/Users/hsiangjenli/Google Drive/My Drive/🏫 2022 NTUST/📦 data/pyg/tgbl-flight"
+	# python test_backup.py --input_folder data/eed --gdrive_path "/Users/hsiangjenli/Google Drive/My Drive/🏫 2022 NTUST/📦 data/pyg/eed"
+	# python test_backup.py --input_folder data/dblp --gdrive_path "/Users/hsiangjenli/Google Drive/My Drive/🏫 2022 NTUST/📦 data/pyg/dblp"
+	python test_backup.py --input_fold data/dblp_1936-2021 --gdrive_path "/Users/hsiangjenli/Google Drive/My Drive/🏫 2022 NTUST/📦 data/pyg/dblp_1936-2021"
+	python test_backup.py --input_fold data/dblp_1936-2022 --gdrive_path "/Users/hsiangjenli/Google Drive/My Drive/🏫 2022 NTUST/📦 data/pyg/dblp_1936-2022"
+	python test_backup.py --input_fold data/dblp_1936-2023 --gdrive_path "/Users/hsiangjenli/Google Drive/My Drive/🏫 2022 NTUST/📦 data/pyg/dblp_1936-2023"
+	python test_backup.py --input_fold data/dblp_1936-2024 --gdrive_path "/Users/hsiangjenli/Google Drive/My Drive/🏫 2022 NTUST/📦 data/pyg/dblp_1936-2024"
 
 all: process backup
---
+++ temporal_graph/preprocess/p_dblp_year.py
@@ -0,0 +1,138 @@
+import numpy as np
+import random
+import pandas as pd
+from itertools import permutations
+from sklearn.preprocessing import OneHotEncoder
+from temporal_graph.base import Preprocessing
+import os
+
+Seperator = ["-", "/", "&", "+"]
+Month = ["january", "february", "march", "april", "may", "june", "july", "august","september", "october", "november", "december"]
+Season = ["spring", "summer", "fall", "winter"]
+Quarter = ["first", "second", "third", "fourth"]
+
+class DBLPPreprocessing(Preprocessing):
+    def __init__(self, input, output, dataset_name, top_n_journal, start_year, end_year) -> None:
+        super().__init__(input=input, output=output, dataset_name=dataset_name, top_n_journal=top_n_journal, start_year=start_year, end_year=end_year)
+    
+    @staticmethod
+    def month_clean(row):
+        if isinstance(row, str):
+            row = row.lower()
+            for s in Seperator:
+                if s in row:
+                    return row.split(s)[-1].strip().lower()
+            
+            for m in Month:
+                if m[:3] in row:
+                    return m
+            
+            for s in Season:
+                if s in row:
+                    return Month[Season.index(s) * 3 + 2]
+            
+            for q in Quarter:
+                if q in row:
+                    return Month[Quarter.index(q) * 3]
+            
+            return row.lower()
+        else:
+            return random.choice(Month)
+
+    @staticmethod
+    def split_author(row):
+        try:
+            return row.split("|")
+        except:
+            return row
+
+    @staticmethod
+    def author_to_list_dir(row):
+        if isinstance(row, list):
+            return [[row[i], row[i+1]] for i in range(len(row)-1)]
+        else:
+            return row
+    
+    def processing(self, dataset_name):
+        df_edge_feat = pd.read_csv(f"{self._input_folder(dataset_name)}/raw_article.csv", sep=";")
+        df_edge_feat = df_edge_feat[["author", "title", "year", "month", "journal"]]
+        df_edge_feat.dropna(inplace=True)
+
+        # -- filter by year
+        df_edge_feat["year"] = df_edge_feat["year"].astype(int)
+        df_edge_feat = df_edge_feat[(df_edge_feat["year"] >= self.start_year) & (df_edge_feat["year"] <= self.end_year)]
+        
+        ohc = OneHotEncoder(handle_unknown='ignore', sparse_output=False, drop='first')
+        top_journal = df_edge_feat["journal"].value_counts().head(self.top_n_journal).index.tolist()
+
+        df_edge_feat["journal"] = df_edge_feat["journal"].apply(lambda x: x if x in top_journal else "Other")
+        ohc.fit(df_edge_feat[["journal"]])
+
+        df_edge_feat["msg"] = ohc.transform(df_edge_feat[["journal"]]).tolist()
+        df_edge_feat["month"] = df_edge_feat["month"].apply(DBLPPreprocessing.month_clean)
+        df_edge_feat["author"] = df_edge_feat["author"].apply(DBLPPreprocessing.split_author)
+
+        df_edge_feat["year"] = df_edge_feat["year"].apply(lambda x: int(x))
+        df_edge_feat["month"] = df_edge_feat["month"].apply(lambda x: Month.index(x.lower()) + 1)
+
+        df_edge_feat["t"] = df_edge_feat["year"] * 1000 + round(df_edge_feat["month"]/12 * 100)
+        df_edge_feat["t"] = df_edge_feat["t"].apply(lambda x: int(x))
+
+        df_edge_feat["author_to_list_dir"] = df_edge_feat["author"].apply(DBLPPreprocessing.author_to_list_dir)
+
+        df_edge_feat = df_edge_feat.explode("author_to_list_dir")
+
+        df_edge_feat.dropna(inplace=True)
+        df_edge_feat.sort_values(by=["t"], inplace=True)
+        df_edge_feat = df_edge_feat.reset_index(drop=True)
+
+        df_edge_feat = pd.concat(
+        [
+            pd.DataFrame(
+                df_edge_feat["author_to_list_dir"].to_list(), columns=["src", "dst"]),
+                df_edge_feat["msg"],
+                df_edge_feat["t"]
+        ], axis=1
+        )
+
+        num_nodes, id_map, src_idx, dst_idx = self._src_dst_to_idx(df_edge_feat["src"], df_edge_feat["dst"])
+
+        msg = np.array(df_edge_feat['msg'].to_list(), dtype=np.float32)
+        msg = msg.astype(float)
+
+        data = {
+            "t": df_edge_feat["t"].to_numpy(),
+            "msg": msg,
+            "src": src_idx, 
+            "dst": dst_idx
+        }
+
+        return data, np.ones((num_nodes, 1), dtype=np.float32)
+    
+    def _output_folder(self, dataset_name) -> str:
+        dataset_name = f"{dataset_name}_{self.start_year}-{self.end_year}"
+        return os.path.join(self._output, dataset_name)
+    
+    def _mask_path(self, dataset_name, mask_type) -> str:
+        return os.path.join(self._output_folder(dataset_name), f"mask_{mask_type}.pkl")
+
+    def _pyg_path(self, dataset_name) -> str:
+        return os.path.join(self._output_folder(dataset_name), f"pyg_{dataset_name}_{self.start_year}-{self.end_year}.pt")
+    
+    def _node_feat_path(self, dataset_name) -> str:
+        return os.path.join(self._output_folder(dataset_name), f"pyg_{dataset_name}_{self.start_year}-{self.end_year}_node_feat.pt")
+
+if __name__ == "__main__":
+    import argparse
+
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--start_year", type=int, default=1936)
+    argparser.add_argument("--end_year", type=int, default=2024)
+
+    args = argparser.parse_args()
+
+    start_year = args.start_year
+    end_year = args.end_year
+
+    dblp = DBLPPreprocessing(input="raw", output="data", dataset_name=f"dblp", top_n_journal=15, start_year=start_year, end_year=end_year)
+    dblp.save()
