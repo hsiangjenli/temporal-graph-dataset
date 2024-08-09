5690d4f8abaf9750fdd20c7817365950ed4073a7
Hsiang-Jen Li <hsiangjenli@gmail.com>
2024-08-09 10:16:02 +0800

    fix: Update train, val, and test masks to use src size instead of num_nodes

--- temporal_graph/utils.py
+++ temporal_graph/utils.py
@@ -3,10 +3,13 @@ from torch_geometric.data import TemporalData
 
 
 def generate_splits(data: TemporalData, val_ratio: float = 0.15, test_ratio: float = 0.15):
-	train_mask = np.zeros(data.num_nodes, dtype=bool)
-	val_mask = np.zeros(data.num_nodes, dtype=bool)
-	test_mask = np.zeros(data.num_nodes, dtype=bool)
+
+	train_mask = np.zeros(data.src.size, dtype=bool)
+	val_mask = np.zeros(data.src.size, dtype=bool)
+	test_mask = np.zeros(data.src.size, dtype=bool)
+
 	train_mask[:10000] = True
 	val_mask[10000:11000] = True
 	test_mask[11000:] = True
+
 	return train_mask.tolist(), val_mask.tolist(), test_mask.tolist()
